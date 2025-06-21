7fe008508436360d400788ee1db1bb00e6597562
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-06-21 15:04:34 -0300

    feat: refatorar AuthService para inicialização da chave secreta no construtor e ajustar geração de token no LoginUseCase

--- src/infrastructure/jwt/auth-service.ts
+++ src/infrastructure/jwt/auth-service.ts
@@ -1,8 +1,12 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
 
 export class AuthService {
-    private secretKey = process.env.JWT_SECRET_KEY || 'S&cr3tK3y';
+    private secretKey: string ;
 
+    constructor() {
+        this.secretKey = process.env.JWT_SECRET || 'S&cr3tK3y';
+    }
+  
     generateToken(userId: string): string {
         const payload = { id: userId };
         return jwt.sign(payload, this.secretKey, { expiresIn: '6h' });
