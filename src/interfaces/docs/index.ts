9140f344f35dec7dccb948c37437e8c51015d7da
Klayton COD.ias <klayton.dias@hotmail.com>
2025-06-21 10:25:44 -0400

    feat: adicionar documentação Swagger para atualização de tarefas

--- src/interfaces/docs/index.ts
+++ src/interfaces/docs/index.ts
@@ -1,9 +1,11 @@
 import { authLoginDoc } from "./auth/auth-login.doc";
 import { authRegisterDoc } from "./auth/auth-register.doc";
 import { taskDoc } from "./task/task-doc.doc";
+import { taskUpdateDoc } from "./task/task-update.doc";
 
 export const swaggerPaths = {
     ...authRegisterDoc,
     ...authLoginDoc,
-    ...taskDoc
+    ...taskDoc,
+    ...taskUpdateDoc
 };
--- src/interfaces/docs/task/task-doc.doc.ts
+++ src/interfaces/docs/task/task-doc.doc.ts
@@ -86,4 +86,5 @@ export const taskDoc = {
         },
       },
     },
+
 }}
