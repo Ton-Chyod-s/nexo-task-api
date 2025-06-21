79a65e50542e1df5ea0db5a6cce4e0f33cff65fc
Klayton COD.ias <klayton.dias@hotmail.com>
2025-06-21 08:35:36 -0400

    feat: adicionar configuração do Swagger para documentação da API

---
+++ src/main/swagger.ts
@@ -0,0 +1,31 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import { swaggerPaths } from "@interfaces/docs";
+
+const swaggerConfig = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Scraper Mailer API",
+      version: "0.1.0",
+      description: "REST API for automation, data scraping, and user management, including official journal and military information handling.",
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+    paths: swaggerPaths,
+  },
+  apis: ["./src/interfaces/routes/*.ts"],
+};
+
+export const swaggerSpec = swaggerJSDoc(swaggerConfig);
