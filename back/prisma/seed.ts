7321e83477c8f67264b56384a43baca8d891c3de
Klayton COD.ias <klayton.dias@hotmail.com>
2025-06-22 00:45:20 -0400

    Refatorar configuração do banco de dados e atualizar schema do Prisma
    Atualizado o arquivo .env para alterar as URLs do banco de dados para desenvolvimento e produção.
    
    Removidos os arquivos antigos do banco SQLite e do schema de desenvolvimento.
    
    Modificados os scripts no package.json para desenvolvimento e produção, incluindo um novo script de deploy.
    
    Atualizado o schema do Prisma para usar PostgreSQL e adicionado um script de seed para a população inicial dos dados.
    
    Criados arquivos de migração para a nova estrutura do banco de dados.

---
+++ back/prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const email = 'klayton.dias@gmail.com';
+
+  const existingUser = await prisma.usuario.findUnique({ where: { email } });
+
+  if (!existingUser) {
+    const user = await prisma.usuario.create({
+      data: {
+        nome: 'Klayton Dias',
+        email,
+        passwordHash: '$2b$10$qXkD1RPzs7X3lUQrzO3IpexDP/DtuPTketQ9W1YuHaDxgKohr6NlC'
+      },
+    });
+
+    console.log('User created:', user);
+  } else {
+    console.log('User already exists:', existingUser);
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
