f166266dc82a892c22df434c14945bc7cb5c6ae4
Klayton COD.ias <klayton.dias@hotmail.com>
2025-06-22 10:41:05 -0400

    feat: reestruturar rotas e adicionar página de Dashboard

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/styles/output.css'
-import App from './App.tsx'
+import App from './routes/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ front/src/pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Painel de Controle</h1>
+    </div>
+  );
+}
